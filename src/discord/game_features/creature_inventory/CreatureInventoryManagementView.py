7514e7f702b7a304a955cd8e331b44a655422cc6
RyanTimas <rtimiss3@gmail.com>
2025-11-18 15:54:10 -0500

    moved creature reward management to a new class

--- src/discord/game_features/creature_inventory/CreatureInventoryManagementView.py
+++ src/discord/game_features/creature_inventory/CreatureInventoryManagementView.py
@@ -3,6 +3,7 @@ from discord.ui import Select
 from src.commons.CommonFunctions import *
 from src.database.handlers.DatabaseHandler import get_tgommo_db_handler
 from src.discord.game_features.creature_inventory.CreatureInventoryImageFactory import CreatureInventoryImageFactory
+from src.discord.game_features.creature_inventory.CreatureReleaseRewardHandler import CreatureReleaseRewardHandler
 from src.discord.game_features.creature_inventory.ReleaseResultImageFactory import ReleaseResultImageFactory
 from src.discord.objects.CreatureRarity import get_rarity_hierarchy_value
 from src.resources.constants.TGO_MMO_constants import *
@@ -41,8 +42,6 @@ class CreatureInventoryManagementView(discord.ui.View):
         self.final_confirmation_button_no = self.create_final_confirmation_button(row=4, is_confirm=False)
         self.select_all_button = self.create_select_all_button(row=4)
         self.deselect_all_button = self.create_deselect_all_button(row=4)
-
-        self.rewardable_items = get_tgommo_db_handler().get_rewardable_inventory_items(convert_to_object=True)
 
         self.refresh_view()
 
@@ -180,7 +179,7 @@ class CreatureInventoryManagementView(discord.ui.View):
 
         for dropdown, is_present in dropdowns:
             if is_present:
-                dropdown.values_ = []
+                dropdown._values = []
 
         self.selected_ids = []
         await interaction.response.edit_message(view=self)
@@ -288,126 +287,12 @@ class CreatureInventoryManagementView(discord.ui.View):
         new_image = self.release_result_image_factory.get_creature_inventory_page_image(currency=currency_earned, earned_items=earned_items, count_released=count_released)
         return convert_to_png(new_image, f'release_results.png')
 
-
-# TODO: MOVE TO SEPARATE FILE
     # REWARD ITEM HANDLING
     def handle_post_release_rewards(self):
-        currency_earned = self.calculate_new_currency_amount()
-        earned_items = self.get_earned_items()
-        return currency_earned,  earned_items
-
-    def calculate_new_currency_amount(self):
-        total_currency = 0
-        for _ in self.selected_ids:
-            total_currency += random.randint(1, 5)
-        return total_currency
-
-    def get_earned_items(self):
-        earned_items = self.get_release_milestone_items()
-
-        # for each released creature, roll for item based on predefined drop rates
-        for selected_id in self.selected_ids:
-            creature = get_tgommo_db_handler().get_creature_by_catch_id(selected_id, convert_to_object=True)
-
-            # roll for random item
-            earned_items.extend(self.get_random_items(creature= creature))
-            # todo: roll for default items
-
-        # Convert to list of tuples (item, count)
-        return self.convert_items_to_count_map(earned_items)
-
-
-    def get_random_items(self, creature):
-        rarity_item_drop_rates = {
-            TGOMMO_RARITY_COMMON: 15,
-            TGOMMO_RARITY_UNCOMMON: 25,
-            TGOMMO_RARITY_NORMAL: 25,
-            TGOMMO_RARITY_RARE: 10,
-            TGOMMO_RARITY_EPIC: 10,
-            TGOMMO_RARITY_LEGENDARY: 5,
-            TGOMMO_RARITY_MYTHICAL: 1,
-            TGOMMO_RARITY_TRANSCENDANT: 1,
-            TGOMMO_RARITY_OMNIPOTENT: 1
-        }
-
-        if random.randint(1, rarity_item_drop_rates[creature.rarity.name]) == 1:
-            if creature.rarity.name == TGOMMO_RARITY_MYTHICAL:
-                return [
-                    get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_LEGENDARY_BAIT, convert_to_object=True),
-                    get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_EPIC_BAIT, convert_to_object=True),
-                    get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_RARE_CHARM, convert_to_object=True),
-                ]
-            if creature.rarity.name == TGOMMO_RARITY_TRANSCENDANT:
-                return [
-                    get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_MYTHICAL_BAIT, convert_to_object=True),
-                    get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_LEGENDARY_BAIT, convert_to_object=True),
-                    get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_EPIC_CHARM, convert_to_object=True),
-                ]
-
-            earned_item = self.roll_for_random_item(creature)
-            return [earned_item,]
-        return []
-    def roll_for_random_item(self, creature):
-        reward_pool = []
-        creature_rarity_hierarchy_value = get_rarity_hierarchy_value(creature.rarity.name)
-
-        rarity_bonuses_rates = {
-            TGOMMO_RARITY_COMMON: 25,
-            TGOMMO_RARITY_UNCOMMON: 25,
-            TGOMMO_RARITY_NORMAL: 15,
-            TGOMMO_RARITY_EPIC: 10,
-            TGOMMO_RARITY_RARE: 10,
-            TGOMMO_RARITY_LEGENDARY: 7,
-            TGOMMO_RARITY_MYTHICAL: 5,
-            TGOMMO_RARITY_TRANSCENDANT: 1,
-            TGOMMO_RARITY_OMNIPOTENT: 1
-        }
-
-        for item in self.rewardable_items:
-            # only throw items at or above the creature's rarity into the pool
-            if item.item_type in (ITEM_TYPE_BAIT, ITEM_TYPE_CHARM):
-                item_rarity_level = get_rarity_hierarchy_value(item.rarity.name)
-
-                if item_rarity_level >= creature_rarity_hierarchy_value:
-                    rate = 1 * rarity_bonuses_rates[item.rarity.name] * (.1 if item.item_type == ITEM_TYPE_CHARM else 1)
-
-                    # perform rarity matching bonus
-                    if creature.rarity.name == item.rarity.name:
-                        rate = 50
-
-                    reward_pool.extend([item] * rate)
-        return reward_pool[random.randint(0, len(reward_pool) -1)]
-
-    def get_release_milestone_items(self):
-        milestone_amounts = {
-            TGOMMO_RARITY_COMMON: 10,
-            TGOMMO_RARITY_UNCOMMON: 25,
-            TGOMMO_RARITY_RARE: 50,
-            TGOMMO_RARITY_EPIC: 100,
-            TGOMMO_RARITY_LEGENDARY: 250,
-            TGOMMO_RARITY_MYTHICAL: 500,
-        }
-
-        user_released_creature_amount = len(get_tgommo_db_handler().get_item_collection_by_user_id(user_id=self.message_author.id, convert_to_object=True))
-        milestone_items = []
-
-        for rarity, milestone in milestone_amounts.items():
-            if user_released_creature_amount % milestone == 0 and user_released_creature_amount > 0:
-                # Find items of this rarity from rewardable items
-                rarity_items = [item for item in self.rewardable_items if item.rarity.name == rarity]
-                if rarity_items:
-                    # Add a random item of this rarity
-                    milestone_items.append(random.choice(rarity_items))
-            elif get_tgommo_db_handler().get_item_collection_by_user_id(user_id=self.message_author.id, convert_to_object=True) == 0:
-                milestone_items.append(get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_LEGENDARY_BAIT, convert_to_object=True))
-                milestone_items.append(get_tgommo_db_handler().get_inventory_item_by_item_id(item_id=ITEM_ID_CHARM, convert_to_object=True))
-
-        return milestone_items
-
-    def convert_items_to_count_map(self, earned_items):
-        # Convert to list of tuples (item, count)
-        item_counts = {}
-        for item in earned_items:
-            item_counts[item] = (item_counts[item] + 1) if item in item_counts else 1
-        return [(item, count) for item, count in item_counts.items()]
+        try:
+            reward_handler = CreatureReleaseRewardHandler(self.message_author.id)
+            return reward_handler.calculate_rewards(self.selected_ids)
+        except Exception as e:
+            print(f"Error calculating rewards: {e}")
+            return 0, {}  # Return default values on error
 

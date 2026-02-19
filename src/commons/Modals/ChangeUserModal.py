d1ff61056fa04069fa4254f44fc534e9c1b9dd59
RyanTimas <rtimiss3@gmail.com>
2026-02-19 14:20:18 -0500

    Refactor ChangeUserModal to improve user profile retrieval and view refresh logic

--- src/commons/Modals/ChangeUserModal.py
+++ src/commons/Modals/ChangeUserModal.py
@@ -16,8 +16,10 @@ class ChangeUserModal(discord.ui.Modal):
             user_id = self.user_id_input.value
             if self.user_id_input.value.strip() == "":
                 user_id = 0 if self.server_access else interaction.user.id
+            new_target_user = get_tgommo_db_handler().get_user_profile_by_user_id(int(user_id))
 
-            reloaded_image = self.view.reload_image(target_user=get_tgommo_db_handler().get_user_profile_by_user_id(int(user_id)))
+            self.view.target_user = new_target_user
+            reloaded_image = self.view.reload_image(target_user=new_target_user)
             self.view.refresh_view()
             await interaction.response.edit_message(attachments=[reloaded_image], view=self.view)
         except ValueError:
